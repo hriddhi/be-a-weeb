d1471a02e5f080cb6284c38eb22e066b28bcb809
hriddhi <35338363+hriddhi@users.noreply.github.com>
2022-11-19 15:03:02 +0530

    Added shop and cart component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ProductsProvider } from "./components/context/products.context";
 import { UserProvider } from "./components/context/user.context";
+import { CartProvider } from "./context/cart.context";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,8 +12,12 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
-      </UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider> 
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,32 +2,40 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import Logo from "../../assets/simbolo_konoha.png";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../components/context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to={"/"}>
-          <img src={Logo} className="logo"></img>
+          <img src={Logo} className="logo" alt="Be-a-weeb logo"></img>
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to={"/shop"}>
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
           ) : (
             <Link className="nav-link" to={"/auth"}>
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        { isCartOpen && <CartDropdown /> }
       </div>
       <Outlet />
     </Fragment>
