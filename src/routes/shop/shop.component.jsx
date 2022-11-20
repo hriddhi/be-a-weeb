9d3d142ffc59a2a691b54fb0f736325d5ba223b5
hriddhi <35338363+hriddhi@users.noreply.github.com>
2022-11-20 14:09:07 +0530

    Implemented Shop preview

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,18 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../context/products.context";
+import { Fragment, useContext } from "react";
+import { CategoriesContext } from "../../context/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
-import './shop.styles.scss'
+import "./shop.styles.scss";
+import CategoryPreview from "../../components/category-perview/category-preview.component";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext)
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map(product => {
-        return (
-          <ProductCard key={product.id} product={product} />
-        );
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return <CategoryPreview key={title} title={title} products={products} />;
       })}
     </div>
   );
