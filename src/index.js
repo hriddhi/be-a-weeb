9c5e3a277e82c11d05008ed8b9eea019d41760e7
hriddhi <35338363+hriddhi@users.noreply.github.com>
2022-11-24 16:26:48 +0530

    Implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { CategoriesProvider } from "./context/categories.context";
-import { UserProvider } from "./context/user.context";
-import { CartProvider } from "./context/cart.context";
+import { PersistGate } from 'redux-persist/integration/react'
 import "./index.scss";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
           <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,21 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middlewares = [ logger ]
 
 const composeEnhancers = compose(applyMiddleware(...middlewares))
 
-export const store = createStore(rootReducer, composeEnhancers)
+export const store = createStore(persistedReducer, composeEnhancers)
+
+export const persistor = persistStore(store)
