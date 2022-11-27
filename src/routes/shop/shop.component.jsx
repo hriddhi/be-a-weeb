41a874307084a804a1cd057787781dc96d9145b1
Hriddhi Mondal <hriddhi1990@gmail.com>
2022-11-27 17:50:13 +0530

    Converted categories-action to redux-saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,15 +2,14 @@ import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/categories.component";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { fetchCategoriesAsync } from "../../store/category/category.action";
+import { fetchCategoriesStart } from "../../store/category/category.action";
 import { useDispatch } from "react-redux";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 import { rootReducer } from "./root-reducer";
 
 import { persistStore, persistReducer } from "redux-persist";
@@ -14,12 +16,16 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [process.env.NODE_ENV !== "production" && logger, sagaMiddleware].filter(
   Boolean
 );
 
 const composeEnhancers = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(persistedReducer, composeEnhancers);
+
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store);
